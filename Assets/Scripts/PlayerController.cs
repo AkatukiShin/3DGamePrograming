fe563e5a3b25bedecaafcb5590be6e9c49f32a59
Kousuke Komiya <kousuke.komy@gmail.com>
2024-10-23 09:32:19 +0900

    ジャンプの調整

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,61 +1,78 @@
 using System;
-using Unity.VisualScripting;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float walkSpeed = 10;
-    [SerializeField] private float runSpeed  = 20;
-    [SerializeField] private float jumpPower = 30;
-    [SerializeField] private float gravityScale = 10;
+    [SerializeField] float walkSpeed = 30;
+    [SerializeField] float dashSpeed = 50;
+    [SerializeField] float jumpPower = 30;
 
     private Rigidbody rigidbody;
 
-    private Vector2 direction;
-    private Vector2 inputMove;
+    private bool isWalk;
+    private bool isDash;
+    private bool isJump;
+    private bool canJump;
 
-    private bool isMove;
-    private bool isDush;
+    private float inputValue;
 
-    private void Start()
+    void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
     }
 
-    private void FixedUpdate()
+    void Update()
     {
-        if (isDush)
+        Move();
+        Jump();
+
+    }
+
+    void Move()
+    {
+        if (isDash && isWalk)
         {
-            rigidbody.AddForce(new Vector2(runSpeed * inputMove.x, 0));
-            Debug.Log("ダッシュ");
+            rigidbody.AddForce(new Vector2(dashSpeed * inputValue, 0));
         }
-
-        if (isMove)
+        else if (isWalk)
         {
-            rigidbody.AddForce(new Vector2(walkSpeed * inputMove.x, 0));
-            Debug.Log("歩く");
+            rigidbody.AddForce(new Vector2(walkSpeed * inputValue, 0));
         }
+    }
 
-        rigidbody.AddForce(new Vector2(0,  -gravityScale));
-}
+    void Jump()
+    {
+        if(canJump)
+        {
+            canJump = false;
+            rigidbody.AddForce(new Vector2(0, jumpPower), ForceMode.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.CompareTag("Ground"))
+        {
+            canJump = true;
+        }
+    }
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        inputValue = context.ReadValue<Vector2>().x;
+        if(context.performed) isWalk = true;
+        if(context.canceled)  isWalk = false;
+    }
     
-    public void Move(InputAction.CallbackContext context)
+    public void OnDash(InputAction.CallbackContext context)
     {
-        inputMove = context.ReadValue<Vector2>();
-        if (context.started)  isMove = true;
-        if (context.canceled) isMove = false;
+        if(context.performed) isDash = true;
+        if(context.canceled)  isDash = false;
     }
-
-    public void Dush(InputAction.CallbackContext context)
+    public void OnJump(InputAction.CallbackContext context)
     {
-        if (context.started)  isDush = true;
-        if (context.canceled) isDush = false;
+        if(context.performed) Jump();
     }
-
-    public void Jump(InputAction.CallbackContext context)
-    {
-        rigidbody.AddForce(new Vector2(0,jumpPower), ForceMode.Impulse);
-    }
-    
 }
