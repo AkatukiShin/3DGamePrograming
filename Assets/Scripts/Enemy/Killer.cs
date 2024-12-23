cf753f3a964c74c5572fef6fb2b527f779f4d733
AkatukiShin <hw23a051@oecu.jp>
2024-12-24 08:38:32 +0900

    うんち

--- Assets/Scripts/Enemy/Killer.cs
+++ Assets/Scripts/Enemy/Killer.cs
@@ -9,6 +9,9 @@ public class Killer : MonoBehaviour
     [SerializeField] private float fireTime = 2.0f;
     [SerializeField] private float speed = 3.0f;
 
+    [SerializeField] private Material serchKillerHead;
+    [SerializeField] private Material defaultKillerHead;
+
     private GameObject player;
     private Rigidbody rb;
     private Vector3 posDelta;
@@ -23,34 +26,105 @@ public class Killer : MonoBehaviour
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        Debug.Log("hello");
     }
 
-    private void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        if (state == 1)
+        Debug.Log("serch");
+        if(InPlayer(other.gameObject))
         {
-            this.transform.parent = null;
-            posDelta = player.transform.position - this.transform.position;
-            this.transform.rotation = Quaternion.LookRotation(posDelta, Vector3.forward);
-            time += Time.deltaTime;
-            if(time >= fireTime)
-            {
-                state = 2;
-            }
+            GameObject head = other.transform.Find("Head").gameObject;
+            head.GetComponent<MeshRenderer>().material = serchKillerHead;
+            Debug.Log("serch");
         }
-        if(state == 2) Move();
+	}
+
+    private void OnTriggerStay(Collider other) 
+    {
+        if(InPlayer(other.gameObject))
+        {
+            //Debug.Log("serch");
+        }
     }
 
-    private void Move()
+    private void OnTriggerExit(Collider other)
     {
-        this.transform.position += posDelta * (speed * Time.deltaTime);
+        if(exitPlayer(other.gameObject))
+        {
+            GameObject head = other.gameObject.transform.GetChild(1).gameObject;
+            head.GetComponent<MeshRenderer>().material = defaultKillerHead;
+        }
     }
 
-    public void DestroyKiller()
+    private bool InPlayer(GameObject other)
     {
-        if(gameObject != null) Destroy(gameObject);
+        if( other.tag == "Player")
+		{
+			Vector3 posDelta = other.transform.position - transform.position;
+			float targetAngle = Vector3.Angle(transform.forward, posDelta);
+            //Debug.Log("視界の範囲内＆視界の角度内＆障害物なし");
+			if( targetAngle < serchAngle)
+			{
+				//if( Physics.Raycaswt(transform.position,new Vector3(posDelta.x,0f,posDelta.z),out RaycastHit hit))
+				//{
+				//	if( hit.collider == other)
+				//	{
+						
+				//	}
+				//}
+                return true;
+			}
+		}
+        return false;
     }
-    
+
+    private bool exitPlayer(GameObject other)
+    {
+        if( other.tag == "Player")
+		{
+			Vector3 posDelta = other.transform.position - transform.position;
+			float targetAngle = Vector3.Angle(transform.forward, posDelta);
+			if( targetAngle >= serchAngle)
+			{
+				//if( Physics.Raycaswt(transform.position,new Vector3(posDelta.x,0f,posDelta.z),out RaycastHit hit))
+				//{
+				//	if( hit.collider == other)
+				//	{
+				//		Debug.Log("視界の範囲内＆視界の角度内＆障害物なし");
+				//	}
+				//}
+                return true;
+			}
+		}
+        return false;
+    }
+    //private void Update()
+    //{
+    //    if (state == 1)
+    //    {
+    //        this.transform.parent = null;
+    //        posDelta = player.transform.position - this.transform.position;
+    //        this.transform.rotation = Quaternion.LookRotation(posDelta, Vector3.forward);
+    //        time += Time.deltaTime;
+    //        if(time >= fireTime)
+    //        {
+    //            state = 2;
+    //        }
+    //    }
+    //    if(state == 2) Move();
+    //}
+//
+    //private void Move()
+    //{
+    //    this.transform.position += posDelta * (speed * Time.deltaTime);
+    //}
+//
+    //public void DestroyKiller()
+    //{
+    //    if(gameObject != null) Destroy(gameObject);
+    //}
+    //
     public int GetSetState
     {
         get { return state;}
@@ -60,32 +134,32 @@ public class Killer : MonoBehaviour
     {
         set { state = value; }
     }
-    
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.CompareTag("Player"))
-        {
-            float targetAngle = Vector3.Angle(this.transform.position, other.gameObject.transform.position);
-            if (targetAngle <= serchAngle)
-            {
-                Debug.Log("serch");
-                player = other.gameObject;
-                state = 1;
-            }
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject.CompareTag("Player"))
-        {
-            float targetAngle = Vector3.Angle(this.transform.position, other.gameObject.transform.position);
-            if (targetAngle > serchAngle)
-            {
-                Debug.Log("lost");
-                state = 0;
-                time = 0;
-            }
-        }
-    }
+    //
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.gameObject.CompareTag("Player"))
+    //    {
+    //        float targetAngle = Vector3.Angle(this.transform.position, other.gameObject.transform.position);
+    //        if (targetAngle <= serchAngle)
+    //        {
+    //            Debug.Log("serch");
+    //            player = other.gameObject;
+    //            state = 1;
+    //        }
+    //    }
+    //}
+//
+    //private void OnTriggerExit(Collider other)
+    //{
+    //    if (other.gameObject.CompareTag("Player"))
+    //    {
+    //        float targetAngle = Vector3.Angle(this.transform.position, other.gameObject.transform.position);
+    //        if (targetAngle > serchAngle)
+    //        {
+    //            Debug.Log("lost");
+    //            state = 0;
+    //            time = 0;
+    //        }
+    //    }
+    //}
 }
